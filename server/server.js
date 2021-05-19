58bff0f498aa36b2df23e0219ae49a16e00558ed
Dheeran Seelam <mindcape@gmail.com>
2021-05-19 06:23:36 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -3,10 +3,25 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
+
+
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/form1":
+            res.writeHead(200);
+            res.end("Success");
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
+    }
 });
 
 server.listen(port, hostname, () => {
